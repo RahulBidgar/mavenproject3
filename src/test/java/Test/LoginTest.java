97ca1d2fcac43d1e55554d7bf6719dd02295676f
Administrator <Administrator@Rahul>
2022-03-31 17:18:34 +0530

    1st comment

---
+++ src/test/java/Test/LoginTest.java
@@ -0,0 +1,52 @@
+package Test;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import Pages.LoginPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {   
+	
+	public static WebDriver driver;
+	LoginPage lp;
+	
+	@BeforeClass
+	public void init() {
+		
+		WebDriverManager.chromedriver().setup();
+		
+		 driver=new ChromeDriver();
+		 
+		 driver.manage().window().maximize();
+		 
+		 driver.get("https://opensource-demo.orangehrmlive.com/");
+		 
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(8));
+			
+			 lp=new LoginPage(driver);
+		
+	
+	}
+	@Test 
+	  public void login() throws InterruptedException {
+	
+		 
+			lp.login("Admin", "admin123");
+			
+	  }
+	
+	@AfterClass
+	public void tearDown() {
+		driver.close();
+	}
+	
+}
+
+
+
